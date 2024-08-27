7078532a8360871b13d95dece6f22b64f3a08766
Rafael de Lima <rafalimabcl@gmail.com>
2024-08-27 00:01:36 -0300

    "feat: add a bad way to handle exception"

--- src/main/java/model/entities/Reservation.java
+++ src/main/java/model/entities/Reservation.java
@@ -39,12 +39,21 @@ public class Reservation {
 
     public long duration() {
         long diff = checkOut.getTime() - checkIn.getTime();
-       return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkIn, Date checkOut) {
-        this.checkIn = checkIn;
-        this.checkOut = checkOut;
+    public String updateDates(Date checkIn, Date checkOut) {
+
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            return "Error in reservation: Reservation dates for update must be future dates";
+        }
+        if (!checkOut.after(checkIn)) {
+            return "Error in reservation: Check-out date must be after check-in date";
+        }
+            this.checkIn = checkIn;
+            this.checkOut = checkOut;
+            return null;
     }
 
     @Override
