2839c0f037ad011cca4d4f6d9ec0b6e202d2343a
Rafael de Lima <rafalimabcl@gmail.com>
2024-08-26 23:38:57 -0300

    chore: create reservation class

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) throws ParseException {
+
+    }
+}
